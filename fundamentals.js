08a7529c5c6ff4a672d9ddf4a6c8d0032e23d1b2
caiyicheng <1619984001@qq.com>
2020-09-29 18:29:49 +0800

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -1,6 +1,10 @@
 
 const aCar = {
     owner: "Joe Bloggs",
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+      ],
     type: {
       make: "Toyota",
       model: "Corolla",
@@ -34,3 +38,9 @@ const aCar = {
       aCar.color.interior.texture +
       " interior."
   );
+  console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  )
